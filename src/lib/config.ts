1187d458871db97cd8daaa34d913cd203e0d366b
Seohyun Lee <me@seorii.page>
2025-08-11 21:12:54 +0900

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 export const compete = true;
 export const selectable = ['2025_1', '2025_2', '2025_3']
-export const def = '2025_2'
+export const def = '2025_3'
 
 export const year = +def.split('_')[0], term = +def.split('_')[1];
